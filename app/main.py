a2fda7613673aeb71a5d233c79c7a45f13532b0e
Samuel Henao <samuhenaog@gmail.com>
2023-11-20 23:10:57 -0500

    Feature: .png with each country's name

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ def run():
   countries = list(map(lambda x: x['Country'], data))
   percentages = list(map(lambda x: x['World Population Percentage'], data))
   charts.generate_pie_chart(countries, percentages)
-  '''
+
   country = input('Type Country => ')
 
   result = utils.population_by_country(data, country)
@@ -17,8 +17,8 @@ def run():
   if len(result) > 0:
     country = result[0]
     labels, values = utils.get_population(country)
-    charts.generate_bar_chart(labels, values)
-  '''
+    charts.generate_bar_chart(country['Country'], labels, values)
+
 
 if __name__ == '__main__':
   run()
